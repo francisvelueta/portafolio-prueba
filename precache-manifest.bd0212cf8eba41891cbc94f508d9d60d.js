2d6a46a5fca1caee57f2d29816360c1fff792916
francisvelueta <fvelueta@outlook.com>
2019-01-14 01:34:57 +0000

    Updates

---
+++ precache-manifest.bd0212cf8eba41891cbc94f508d9d60d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "54afa65d4c3750d4c6af583293be79f7",
+    "url": "/static/media/perfil.54afa65d.jpeg"
+  },
+  {
+    "revision": "284332f518c6a66d89372fd973f6f3cb",
+    "url": "/static/media/logogeek.284332f5.png"
+  },
+  {
+    "revision": "da1f326682c64acacb9edcc49c33d116",
+    "url": "/static/media/CVFrancis.da1f3266.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6539d289b66bd4fcd741",
+    "url": "/static/js/main.6539d289.chunk.js"
+  },
+  {
+    "revision": "dcd7d101bb09a32d0efb",
+    "url": "/static/js/1.dcd7d101.chunk.js"
+  },
+  {
+    "revision": "6539d289b66bd4fcd741",
+    "url": "/static/css/main.9b045a9f.chunk.css"
+  },
+  {
+    "revision": "dcd7d101bb09a32d0efb",
+    "url": "/static/css/1.301970d2.chunk.css"
+  },
+  {
+    "revision": "e53e674030c3c37c98f5495b732d4dbb",
+    "url": "/index.html"
+  }
+];
